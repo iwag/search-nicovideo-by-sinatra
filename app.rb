d4ddf555bcf6fbe3eba37b4b7d0513d2fe8b2ab1
iwag <iwagame@gmail.com>
2015-11-21 18:31:19 +0900

    kensaku mitame

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ get '/search' do
   hits = []
   if (keyword != nil && !keyword.empty?) 
     qb = SearchNicovideo::QueryBuilder.new()
-    filters =  [
-#      SearchNicovideo::FilterBuilder.new().type("equal").field("categoryTags").value("ゲーム").build(),
-    ]
-    q = qb.query(keyword).service(["video"]).search(["title","description"]).join(["title","description","start_time","content_id"]).filters(filters).sort_by("start_time").desc(false).build().to_json
+    filters =  []
+    q = qb.query(keyword).service(["video"]).search(["title","description"])
+      .join(["title","description","start_time","content_id", "thumbnail_url"])
+      .filters(filters).sort_by("start_time").desc(true).build().to_json
     hits = SearchNicovideo::search(q)[:hits]
     hits = (hits==nil || hits.empty? || hits[0] == nil || hits[0]["values"] == nil) ?  [] : hits[0]["values"]
   end
