4ab51f31ba3571cb44e95b37ef3079a9ca594fc9
iwag <iwagame@gmail.com>
2015-11-21 18:22:20 +0900

    kensaku

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'search-nicovideo-rb', git: 'https://github.com/iwag/search-nicovideo-rb.git'
--- app.rb
+++ app.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
+require 'nicosearch'
 
 get '/' do
   erb :search
 end
 
-post '/search' do
-  erb :search
+get '/search' do
+  keyword = params[:q]
+  hits = []
+  if (keyword != nil && !keyword.empty?) 
+    qb = SearchNicovideo::QueryBuilder.new()
+    filters =  [
+#      SearchNicovideo::FilterBuilder.new().type("equal").field("categoryTags").value("ゲーム").build(),
+    ]
+    q = qb.query(keyword).service(["video"]).search(["title","description"]).join(["title","description","start_time","content_id"]).filters(filters).sort_by("start_time").desc(false).build().to_json
+    hits = SearchNicovideo::search(q)[:hits]
+    hits = (hits==nil || hits.empty? || hits[0] == nil || hits[0]["values"] == nil) ?  [] : hits[0]["values"]
+  end
+  erb :search, locals: {keyword: keyword, hits: hits}
 end
 
