8cc3fdcc66dddbb14c9fd2824ebf8a232da6aacb
iwag <iwagame@gmail.com>
2015-11-21 20:39:55 +0900

    support nico search new version

--- app.rb
+++ app.rb
@@ -11,11 +11,11 @@ get '/search' do
   if (keyword != nil && !keyword.empty?) 
     qb = SearchNicovideo::QueryBuilder.new()
     filters =  []
-    q = qb.query(keyword).service(["video"]).search(["title","description"])
-      .join(["title","description","start_time","content_id", "thumbnail_url"])
-      .filters(filters).sort_by("start_time").desc(true).build().to_json
-    hits = SearchNicovideo::search(q)[:hits]
-    hits = (hits==nil || hits.empty? || hits[0] == nil || hits[0]["values"] == nil) ?  [] : hits[0]["values"]
+    q = qb.query(keyword).service(["video"]).targets(["title","description"])
+      .fields(["title","description","startTime","contentId", "thumbnailUrl"])
+      .size(10).from(0)
+      .filters(filters).sort("startTime").build
+    hits = SearchNicovideo::search(q)["data"]
   end
   erb :search, locals: {keyword: keyword, hits: hits}
 end
